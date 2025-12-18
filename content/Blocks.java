a4d183065460e5af44412c540780224aaf11326c
SomeNameMindustry <emailemailemailemaildiscord@gmail.com>
2025-12-18 15:47:41 -0300

    Update Blocks.java

--- content/Blocks.java
+++ content/Blocks.java
@@ -1,15 +1,1 @@
-block.fuse.ammoTypes: {
-  //remove titanium ammo from the ammo map by using the special "-" value
-  titanium: "-"
-  //add surge alloy ammo that shoots a laser
-  surge-alloy: {
-    type: LaserBulletType
-    //make it produce 1 shot per ammo item
-    ammoMultiplier: 1
-    //make it shoot half as fast
-    reloadMultiplier: 0.5
-    damage: 100
-    //make it look awful!
-    colors: ["000000", "ff0000", "ffffff"]
-  }
-}
+
